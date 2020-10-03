2a9aeee97208a80d6db875497eea22f644419c29
Xuân Nguyễn <xuan123dthhbg@gmail.com>
2020-10-03 10:28:31 +0700

    q 1 q2 q3 q4

--- search.py
+++ search.py
@@ -87,16 +87,96 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
+    frontier = util.Stack()
+    exploredNodes = []
+    startState = problem.getStartState()
+    startNode = (startState, [])
+    frontier.push(startNode)
+    
+    while not frontier.isEmpty():
+        currentState, actions = frontier.pop()
+        
+        if currentState not in exploredNodes:
+            exploredNodes.append(currentState)
+
+            if problem.isGoalState(currentState):
+                return actions
+            else:
+                successors = problem.getSuccessors(currentState)
+                for succState, succAction, succCost in successors:
+                    newAction = actions + [succAction]
+                    newNode = (succState, newAction)
+                    frontier.push(newNode)
+
+    return actions  
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
+     #to be explored (FIFO)
+    frontier = util.Queue()
+    
+    #previously expanded states (for cycle checking), holds states
+    exploredNodes = []
+    
+    startState = problem.getStartState()
+    startNode = (startState, [], 0)
+    
+    frontier.push(startNode)
+    
+    while not frontier.isEmpty():
+        currentState, actions, currentCost = frontier.pop()
+        
+        if currentState not in exploredNodes:
+            exploredNodes.append(currentState)
+
+            if problem.isGoalState(currentState):
+                return actions
+            else:
+                successors = problem.getSuccessors(currentState)
+                
+                for succState, succAction, succCost in successors:
+                    newAction = actions + [succAction]
+                    newCost = currentCost + succCost
+                    newNode = (succState, newAction, newCost)
+
+                    frontier.push(newNode)
+
+    return actions
     util.raiseNotDefined()
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
+    frontier = util.PriorityQueue()
+
+    exploredNodes = {}
+    
+    startState = problem.getStartState()
+    startNode = (startState, [], 0) #(state, action, cost)
+    
+    frontier.push(startNode, 0)
+    
+    while not frontier.isEmpty():
+        currentState, actions, currentCost = frontier.pop()
+       
+        if (currentState not in exploredNodes) or (currentCost < exploredNodes[currentState]):
+            exploredNodes[currentState] = currentCost
+
+            if problem.isGoalState(currentState):
+                return actions
+            else:
+                successors = problem.getSuccessors(currentState)
+                
+                for succState, succAction, succCost in successors:
+                    newAction = actions + [succAction]
+                    newCost = currentCost + succCost
+                    newNode = (succState, newAction, newCost)
+
+                    frontier.update(newNode, newCost)
+
+    return actions
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
@@ -109,6 +189,44 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+    frontier = util.PriorityQueue()
+
+    exploredNodes = [] #holds (state, cost)
+
+    startState = problem.getStartState()
+    startNode = (startState, [], 0) #(state, action, cost)
+
+    frontier.push(startNode, 0)
+
+    while not frontier.isEmpty():
+        currentState, actions, currentCost = frontier.pop()
+        currentNode = (currentState, currentCost)
+        exploredNodes.append((currentState, currentCost))
+
+        if problem.isGoalState(currentState):
+            return actions
+
+        else:
+            successors = problem.getSuccessors(currentState)
+
+            for succState, succAction, succCost in successors:
+                newAction = actions + [succAction]
+                newCost = problem.getCostOfActions(newAction)
+                newNode = (succState, newAction, newCost)
+
+                already_explored = False
+                for explored in exploredNodes:
+                    exploredState, exploredCost = explored
+
+                    if (succState == exploredState) and (newCost >= exploredCost):
+                        already_explored = True
+
+                if not already_explored:
+                    frontier.push(newNode, newCost + heuristic(succState, problem))
+                    exploredNodes.append((succState, newCost))
+
+    return actions
+
     util.raiseNotDefined()
 
 
